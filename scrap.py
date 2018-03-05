be3beead28ba02550bd7f89d92c3d992294cea9f
Pere Piñol <ppp3@alumnes.udl.cat>
2018-03-05 12:20:19 +0100

    Added comments.

--- scrap.py
+++ scrap.py
@@ -2,25 +2,44 @@ from bs4 import BeautifulSoup
 import urllib2
 
 
+"""
+	Prints the given elements in this format:
+	<title>
+		<subtitle>
+		* <description[0]>
+		* <description[1]>
+		...
+"""
 def print_info(title, subtitle, description):
+	
     print("\n" + title + "\n")
     print("\t" + subtitle + "\n")
     for item in description:
         print("\t* " + str(item.text.strip().encode('ascii', 'ignore')) + "\n")
 
 
-if __name__ == "__main__":
-    html = urllib2.urlopen("https://www.packtpub.com/packt/offers/"
-                           + "free-learning/").read()
+"""
+	Accesses PACKT free learning book webpage and gets the book's title,
+	subtitle and general information, printing it.
+"""
+def scrap():
+
+    html = urllib2.urlopen("https://www.packtpub.com/packt/offers/" +\
+                           "free-learning/").read()
     soup = BeautifulSoup(html, "lxml")
 
     # Get title
     title = soup.find("h2")
 
-    # Get subtitle
+    # Get subtitle (by navigating from title)
     subtitle = title.parent.parent.find_all("div")[2].text
 
     # Get bullet points
     bullet_points = soup.find_all("li")
 
     print_info(title.text.strip(), subtitle.strip(), bullet_points)
+
+
+
+if __name__ == "__main__":
+    scrap()
